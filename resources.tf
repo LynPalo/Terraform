a6978ecba98a631a7c147c5fade8010eacd4f5b7
Sean <sean@Seans-MBP.fios-router.home>
2019-06-15 18:14:15 -0400

    security group

--- resources.tf
+++ resources.tf
@@ -1,20 +1,33 @@
 resource "aws_vpc" "example" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
+
   tags {
-      Name = "terraform-example"
+    Name = "terraform-example"
   }
 }
 
 resource "aws_subnet" "subnet_1" {
-  cidr_block = "${cidrsubnet(aws_vpc.example.cidr_block, 3, 1)}"
-  vpc_id = "${aws_vpc.example.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.example.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.example.id}"
   availability_zone = "us-east-1a"
 }
 
 resource "aws_subnet" "subnet_2" {
-  cidr_block = "${cidrsubnet(aws_vpc.example.cidr_block, 2, 2)}"
-  vpc_id = "${aws_vpc.example.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.example.cidr_block, 2, 2)}"
+  vpc_id            = "${aws_vpc.example.id}"
   availability_zone = "us-east-1b"
+}
+
+resource "aws_security_group" "subnet_security_group" {
+  vpc_id = "${aws_vpc.example.id}"
+
+  ingress {
+    cidr_blocks = ["${aws_vpc.example.cidr_block}"]
+
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+  }
 }
