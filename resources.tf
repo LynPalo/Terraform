199e759c80837d9dcf8ff99ea10c28dddee0581c
Sean <sean@Seans-MBP.fios-router.home>
2019-06-15 17:55:33 -0400

    subnet2

--- resources.tf
+++ resources.tf
@@ -12,3 +12,9 @@ resource "aws_subnet" "subnet_1" {
   vpc_id = "${aws_vpc.example.id}"
   availability_zone = "us-east-1a"
 }
+
+resource "aws_subnet" "subnet_2" {
+  cidr_block = "${cidrsubnet(aws_vpc.example.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.example.id}"
+  availability_zone = "us-east-1b"
+}
