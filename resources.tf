f9ff2ac548d8c0ad1b33f34427b7c6f572993dba
Sean <sean@Seans-MBP.fios-router.home>
2019-06-15 17:44:44 -0400

    subnet

--- resources.tf
+++ resources.tf
@@ -6,3 +6,9 @@ resource "aws_vpc" "example" {
       Name = "terraform-example"
   }
 }
+
+resource "aws_subnet" "subnet_1" {
+  cidr_block = "${cidrsubnet(aws_vpc.example.cidr_block, 3, 1)}"
+  vpc_id = "${aws_vpc.example.id}"
+  availability_zone = "us-east-1a"
+}
