968c1b13ef24e97ff0dfaf4acf4ce3cf58763202
Sean <sean@Seans-MBP.fios-router.home>
2019-06-15 21:55:36 -0400

    server

--- resources.tf
+++ resources.tf
@@ -31,3 +31,31 @@ resource "aws_security_group" "subnet_security_group" {
     protocol  = "tcp"
   }
 }
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+      name = "name"
+      values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+      name = "virtualization-type"
+      values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+resource "aws_instance" "server_2" {
+    ami = "${data.aws_ami.ubuntu.id}"
+    instance_type = "t2.micro"
+
+    tags {
+        Name = "ec2"
+    }
+  subnet_id = "${aws_subnet.subnet_1.id}"
+}
+
+
